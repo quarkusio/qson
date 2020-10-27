dc009ea957f6f579c471b0dcf061c86023789230
Bill Burke <bburke@redhat.com>
2020-10-27 16:22:31 -0400

    decoding

--- core/src/main/java/io/quarkus/qson/desserializer/ParsePrimitives.java
+++ core/src/main/java/io/quarkus/qson/desserializer/ParsePrimitives.java
@@ -1,17 +1,105 @@
 package io.quarkus.qson.desserializer;
 
+import java.util.Arrays;
+
 import static io.quarkus.qson.IntChar.*;
 
 public class ParsePrimitives {
     static int[] TRUE_VALUE = {INT_t, INT_r, INT_u, INT_e};
     static int[] FALSE_VALUE = {INT_f, INT_a, INT_l, INT_s, INT_e};
 
+    /*
     public static String readString(byte[] buffer, int tokenStart, int tokenEnd) {
         char[] charbuf = new char[tokenEnd - tokenStart];
         for (int i = 0; i < tokenEnd - tokenStart; i++) charbuf[i] = (char)(buffer[tokenStart + i] & 0xFF);
         return new String(charbuf);
     }
+    */
 
+    public static String readString(byte[] buffer, int tokenStart, int tokenEnd) {
+        char[] charbuf = new char[tokenEnd - tokenStart];
+        int count = 0;
+        int ptr = tokenStart;
+        while (ptr < tokenEnd) {
+            int c = buffer[ptr++] & 0xFF;
+            if (c == '\\') {
+                c = buffer[ptr++] & 0xFF;
+                boolean encoded = false;
+                switch (c) {
+                    // First, ones that are mapped
+                    case 'b':
+                        charbuf[count++] = '\b';
+                        break;
+                    case 't':
+                        charbuf[count++] = '\t';
+                        break;
+                    case 'n':
+                        charbuf[count++] = '\n';
+                        break;
+                    case 'f':
+                        charbuf[count++] = '\f';
+                        break;
+                    case 'r':
+                        charbuf[count++] = '\r';
+                        break;
+
+                    // And these are to be returned as they are
+                    case '"':
+                    case '/':
+                    case '\\':
+                        charbuf[count++] = (char) c;
+                        break;
+
+                    case 'u': // and finally hex-escaped
+                        encoded = true;
+                        break;
+
+                    default:
+                        throw new RuntimeException("Unknown character format in string");
+                }
+
+                if (encoded) {
+                    // Ok, a hex escape. Need 4 characters
+                    int value = 0;
+                    for (int i = 0; i < 4; ++i) {
+                        int ch = buffer[ptr++] & 0xFF;
+                        int digit = sHexValues[ch & 0xFF];
+                        if (digit < 0) {
+                            throw new RuntimeException("expected a hex-digit for character escape sequence");
+                        }
+                        value = (value << 4) | digit;
+                    }
+                    charbuf[count++] = (char) value;
+                }
+            } else {
+                int tmp = c & 0xF0; // mask out top 4 bits to test for multibyte
+                if (tmp == 0xC0) {
+                    // 2 byte
+                } else if (tmp == 0xE0) {
+                    // 3 byte
+                } else if (tmp == 0xF0) {
+                    // 4 byte
+                } else {
+                    charbuf[count++] = (char) c;
+                }
+            }
+
+        }
+        return new String(charbuf, 0, count);
+    }
+
+    private final static int[] sHexValues = new int[256];
+
+    static {
+        Arrays.fill(sHexValues, -1);
+        for (int i = 0; i < 10; ++i) {
+            sHexValues['0' + i] = i;
+        }
+        for (int i = 0; i < 6; ++i) {
+            sHexValues['a' + i] = 10 + i;
+            sHexValues['A' + i] = 10 + i;
+        }
+    }
 
 
     public static boolean readBoolean(byte[] buffer, int tokenStart, int tokenEnd) {
@@ -20,14 +108,14 @@ public class ParsePrimitives {
         int len = tokenEnd - tokenStart;
         if (len == 4) {
             for (int i = 0; i < 4; i++) {
-                if (TRUE_VALUE[i] != ((int)buffer[tokenStart + i] & 0xFF)) {
+                if (TRUE_VALUE[i] != ((int) buffer[tokenStart + i] & 0xFF)) {
                     break;
                 }
             }
             return true;
         } else if (len == 5) {
             for (int i = 0; i < 5; i++) {
-                if (FALSE_VALUE[i] != ((int)buffer[tokenStart + i] & 0xFF)) {
+                if (FALSE_VALUE[i] != ((int) buffer[tokenStart + i] & 0xFF)) {
                     break;
                 }
             }
@@ -61,18 +149,18 @@ public class ParsePrimitives {
                 ++i;
             }
 
-            long multmin = limit / (long)10;
+            long multmin = limit / (long) 10;
 
             long result;
             int digit;
-            for(result = 0L; i < len; result -= (long)digit) {
+            for (result = 0L; i < len; result -= (long) digit) {
                 digit = (buffer[i++ + tokenStart] & 0xFF) - INT_0;
                 if (digit < 0 || result < multmin) {
                     throw new RuntimeException("Illegal number format");
                 }
 
-                result *= (long)10;
-                if (result < limit + (long)digit) {
+                result *= (long) 10;
+                if (result < limit + (long) digit) {
                     throw new RuntimeException("Illegal number format");
                 }
             }
---
+++ core/src/test/java/io/quarkus/qson/test/DecodingTest.java
@@ -0,0 +1,24 @@
+package io.quarkus.qson.test;
+
+import io.quarkus.qson.desserializer.ParsePrimitives;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.UnsupportedEncodingException;
+
+public class DecodingTest {
+
+    @Test
+    public void testDecoding() throws Exception{
+        String string = "hello";
+        decode("hello", "hello");
+        decode("hello\\nworld", "hello\nworld");
+        decode("hello\\\"world\\\"", "hello\"world\"");
+    }
+
+    private void decode(String string, String expected) throws UnsupportedEncodingException {
+        byte[] bytes = string.getBytes("UTF-8");
+        String decoded = ParsePrimitives.readString(bytes, 0, bytes.length);
+        Assertions.assertEquals(expected, decoded);
+    }
+}

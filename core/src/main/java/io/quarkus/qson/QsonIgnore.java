cd7a8186fc299bf338831aad5a707ef7d53cdb52
Bill Burke <bburke@redhat.com>
2020-11-16 13:25:32 -0500

    javadoc

--- core/src/main/java/io/quarkus/qson/QsonIgnore.java
+++ core/src/main/java/io/quarkus/qson/QsonIgnore.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Ignore a property for serialization or deserialization
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface QsonIgnore {
--- core/src/main/java/io/quarkus/qson/QsonProperty.java
+++ core/src/main/java/io/quarkus/qson/QsonProperty.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Can set the json property name.
+ * Can set visibility of property too.
+ *
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface QsonProperty {
@@ -13,8 +18,18 @@ public @interface QsonProperty {
 
     public enum Serialization
     {
+        /**
+         * serialize and deserialize if there is a corresponding getter and setter
+         */
         DEFAULT,
+        /**
+         * If property has a setter, then ignore the property on deserialization
+         *
+         */
         SERIALIZED_ONLY,
+        /**
+         * If property has a getter, then ignore it for serialization
+         */
         DESERIALIZED_ONLY
         ;
     }

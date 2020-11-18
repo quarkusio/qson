1e8e9665f6c743585ce465e4e75f5eaf329487b8
Bill Burke <bburke@redhat.com>
2020-11-18 10:23:01 -0500

    capability

--- quarkus/resteasy-qson/deployment/src/main/java/io/quarkus/qson/resteasy/deployment/QsonJaxrsBuildStep.java
+++ quarkus/resteasy-qson/deployment/src/main/java/io/quarkus/qson/resteasy/deployment/QsonJaxrsBuildStep.java
@@ -1,8 +1,12 @@
 package io.quarkus.qson.resteasy.deployment;
 
 import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
+import io.quarkus.deployment.Capability;
+import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CapabilityBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.qson.util.Types;
 import io.quarkus.qson.deployment.QsonBuildItem;
 import io.quarkus.resteasy.common.spi.ResteasyDotNames;
@@ -26,6 +30,16 @@ import java.util.List;
 import java.util.Set;
 
 public class QsonJaxrsBuildStep {
+
+    @BuildStep
+    void feature(BuildProducer<FeatureBuildItem> feature) {
+        feature.produce(new FeatureBuildItem("resteasy-qson"));
+    }
+
+    @BuildStep
+    void capabilities(BuildProducer<CapabilityBuildItem> capability) {
+        capability.produce(new CapabilityBuildItem(Capability.RESTEASY_JSON));
+    }
 
     @BuildStep
     public void findQsonClasses(BeanArchiveIndexBuildItem beanArchiveIndexBuildItem,
